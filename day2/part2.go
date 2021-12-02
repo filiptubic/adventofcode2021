46ff6d67c8b9b4fd19aa9dab17ec24221a0c7299
filip <filip@pop-os.localdomain>
2021-12-02 07:25:42 +0100

    add day2 solution

---
+++ day2/part2.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type action struct {
+	path  string
+	steps int
+}
+
+func loadInput(path string) ([]action, error) {
+	input := make([]action, 0)
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+		path, steps := parts[0], parts[1]
+
+		stepsNum, err := strconv.Atoi(steps)
+		if err != nil {
+			return nil, err
+		}
+		input = append(input, action{path: path, steps: stepsNum})
+	}
+	return input, nil
+}
+func main() {
+	actions, err := loadInput("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	x, depth, aim := 0, 0, 0
+	for _, action := range actions {
+		switch action.path {
+		case "forward":
+			x += action.steps
+			depth += action.steps * aim
+		case "down":
+			aim += action.steps
+		case "up":
+			aim -= action.steps
+		}
+		fmt.Println(x, depth, aim)
+	}
+	fmt.Println(x * depth)
+}
